26a769b7ec0d59bb65b52c0d6bc3a891f828a2c4
PalaghitaAndrei <109868729+PalaghitaAndrei@users.noreply.github.com>
2023-04-27 11:38:28 +0300

    am schimbat email-ul

--- GetYoBike/Server/Controllers/UsersController.cs
+++ GetYoBike/Server/Controllers/UsersController.cs
@@ -1,7 +1,12 @@
-﻿using GetYoBike.Server.Data;
-using GetYoBike.Server.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using GetYoBike.Server.Data;
+using GetYoBike.Server.Models;
 
 namespace GetYoBike.Server.Controllers
 {
@@ -20,21 +25,21 @@ namespace GetYoBike.Server.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
             return await _context.Users.ToListAsync();
         }
 
         // GET: api/Users/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(string id)
+        public async Task<ActionResult<User>> GetUser(int id)
         {
-            if (_context.Users == null)
-            {
-                return NotFound();
-            }
+          if (_context.Users == null)
+          {
+              return NotFound();
+          }
             var user = await _context.Users.FindAsync(id);
 
             if (user == null)
@@ -48,7 +53,7 @@ namespace GetYoBike.Server.Controllers
         // PUT: api/Users/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(string id, User user)
+        public async Task<IActionResult> PutUser(int id, User user)
         {
             if (id != user.Id)
             {
@@ -81,33 +86,19 @@ namespace GetYoBike.Server.Controllers
         [HttpPost]
         public async Task<ActionResult<User>> PostUser(User user)
         {
-            if (_context.Users == null)
-            {
-                return Problem("Entity set 'DataContext.Users'  is null.");
-            }
+          if (_context.Users == null)
+          {
+              return Problem("Entity set 'DataContext.Users'  is null.");
+          }
             _context.Users.Add(user);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (UserExists(user.Id))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetUser", new { id = user.Id }, user);
         }
 
         // DELETE: api/Users/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUser(string id)
+        public async Task<IActionResult> DeleteUser(int id)
         {
             if (_context.Users == null)
             {
@@ -125,7 +116,7 @@ namespace GetYoBike.Server.Controllers
             return NoContent();
         }
 
-        private bool UserExists(string id)
+        private bool UserExists(int id)
         {
             return (_context.Users?.Any(e => e.Id == id)).GetValueOrDefault();
         }
--- GetYoBike/Server/Models/User.cs
+++ GetYoBike/Server/Models/User.cs
@@ -2,7 +2,7 @@
 {
     public class User
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Email { get; set; }
         public string Last_Name { get; set; }
         public string First_Name { get; set; }
