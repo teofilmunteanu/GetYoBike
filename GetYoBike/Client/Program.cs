b5d29e76eb14afb22a8112c2d7670781537ff5fe
Munteanu Teofil <andreiteofil01@gmail.com>
2023-10-02 03:54:55 +0300

    Edit page working
    TO DO:
    -handle all exceptions
    -user bike types properly (by strings, and types, not nr)

--- GetYoBike/Client/Program.cs
+++ GetYoBike/Client/Program.cs
@@ -9,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<RentService>();
+builder.Services.AddScoped<BikeService>();
 
 await builder.Build().RunAsync();
---
+++ GetYoBike/Client/Services/BikeService.cs
@@ -0,0 +1,55 @@
+﻿using GetYoBike.Shared.Models;
+using System.Net.Http.Json;
+
+namespace GetYoBike.Client.Services
+{
+    public class BikeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BikeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<BikeModel> GetAnAvailableBike(DateTime dateTimeStart, DateTime dateTimeEnd, int bikeTypeNr)
+        {
+            string startDateTimeString = dateTimeStart.ToString("yyyy-MM-dd-HH-mm");
+            string endDateTimeString = dateTimeEnd.ToString("yyyy-MM-dd-HH-mm");
+
+            try
+            {
+                List<BikeModel>? availableBikes = await _httpClient.GetFromJsonAsync<List<BikeModel>>(
+                    $"api/Bikes/availableBikesInInterval?" +
+                    $"startDateTime={startDateTimeString}" +
+                    $"&endDateTime={endDateTimeString}" +
+                    $"&bikeTypeId={bikeTypeNr}"
+                );
+
+                if (availableBikes != null)
+                {
+                    int randBikeId = new Random().Next(0, availableBikes.Count - 1);
+                    BikeModel bike = availableBikes[randBikeId];
+
+                    return bike;
+                }
+                else
+                {
+                    throw new Exception("Couldn't check for available bikes!");
+                }
+
+            }
+            catch (HttpRequestException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    throw new Exception("No bikes available");
+                }
+                else
+                {
+                    throw e;
+                }
+            }
+        }
+    }
+}
